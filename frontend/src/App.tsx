15de033d29ea6ecc3229c5c4acd96edadf4c50bb
Fernando Serpa <serpaef@outlook.com>
2025-10-20 20:18:57 -0300

    feat: update routing in App component to include login and create pessoa pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-
-function Home() {
-  return <h1>Bem-vindo!</h1>;
-}
+import LogoffButton from "./components/LogoffButton";
+import { Home } from "./pages/ListPessoaPage";
+import CreatePessoaPage from "./pages/CreatePessoaPage";
 
 export default function App() {
   return (
     <Router>
+      <div className="p-4 flex justify-end">
+        <LogoffButton />
+      </div>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/cadastrar" element={<CreatePessoaPage />} />
       </Routes>
     </Router>
   );
